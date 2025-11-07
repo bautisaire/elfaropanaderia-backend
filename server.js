e7705f45d84d586ae8b770070075b519a866bd8e
Misaianes <sairebautista@gmail.com>
2025-11-07 19:59:17 -0300

    asdas

--- server.js
+++ server.js
@@ -6,25 +6,39 @@ import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
 const allowedOrigins = [
-  "https://elfaropanaderia.vercel.app/", // tu dominio de Vercel
+  "https://elfaropanaderia.vercel.app", // tu dominio de Vercel
   "http://localhost:5000" // para desarrollo local 
 ];
 
 const app = express();
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true, 
-}));
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 app.use(express.json());
-
+// ✅ Ruta de prueba
+app.get("/", (req, res) => {
+  res.send("🍞 API funcionando correctamente 🚀");
+});
 // Rutas
 app.use("/api/orders", orderRoutes);
 
 // Conectar a MongoDB
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     console.log("✅ Conectado a MongoDB Atlas");
+//     app.listen(5000, () => console.log("🚀 Servidor backend en puerto 5000"));
+//   })
+//   .catch((err) => console.error("❌ Error al conectar MongoDB:", err));
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ Conectado a MongoDB Atlas");
-    app.listen(5000, () => console.log("🚀 Servidor backend en puerto 5000"));
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`🚀 Servidor backend en puerto ${PORT}`));
   })
   .catch((err) => console.error("❌ Error al conectar MongoDB:", err));
